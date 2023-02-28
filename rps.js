fbe13f554080ef2e2eec65843f6458a7580f65fb
Jack Lingle <captainjack0818@gmail.com>
2023-02-28 13:37:28 -0500

    Add get winner and player choice functions

--- rps.js
+++ rps.js
@@ -1,11 +1,23 @@
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     if (random === 2) {
-        return 'Rock';
+        return 'rock';
     } else if (random === 1) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
-getComputerChoice()
+function getPlayerChoice() {
+    let choice = prompt("Rock, Paper, or Scissors?");
+    return choice.toLowerCase();
+}
+function getWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return 'tie!';
+    } else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
+        return 'Player wins! ' + playerChoice + ' beats ' + computerChoice;
+    } else {
+        return 'Computer wins! ' + computerChoise + ' beats ' + playerChoice;
+    }
+}
