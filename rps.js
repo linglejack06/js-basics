41f8238b76c174efb954089c8c7f7e5c4f061f4b
Jack Lingle <captainjack0818@gmail.com>
2023-03-01 12:13:42 -0500

    Add game function to display winner

--- rps.js
+++ rps.js
@@ -16,8 +16,29 @@ function getWinner(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         return 'tie!';
     } else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
-        return 'Player wins! ' + playerChoice + ' beats ' + computerChoice;
+        return 'player';
     } else {
-        return 'Computer wins! ' + computerChoise + ' beats ' + playerChoice;
+        return 'computer';
     }
 }
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let winner = getWinner(getPlayerChoice(), getComputerChoice());
+        console.log(winner);
+        if (winner === 'player') {
+            playerScore++;
+        } else if (winner === 'computer') {
+            computerScore++;
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log('Player Wins: ' + playerScore + '-' + computerScore);
+    } else if (computerScore > playerScore) {
+        console.log('Computer Wins: ' + computerScore + '-' + playerScore);
+    } else {
+        console.log('tie: ' + playerScore + '-' + computerScore);
+    }
+}
+game()
